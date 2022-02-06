cc2b098eff733bfc5b15a5bf305c04b3fdb3337e
Haleem Hosein <haleemhosein@yahoo.com>
2022-02-05 20:45:27 -0400

    08 Appointments in the database -- listen for model creating event and add uuid and token

---
+++ app/Models/Appointment.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Appointment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'date',
+        'start_time',
+        'end_time',
+        'client_name',
+        'client_email',
+    ];
+
+    protected $casts = [
+        'date' => 'datetime',
+        'start_time' => 'datetime',
+        'end_time' => 'datetime',
+    ];
+
+    public static function booted()
+    {
+        static::creating(function ($model) {
+            $model->uuid = Str::uuid();
+            $model->token = Str::random(32);
+        });
+    }
+
+    public function service()
+    {
+        return $this->belongsTo(Service::class);
+    }
+
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class);
+    }
+}
---
+++ database/migrations/2022_02_05_202921_create_appointments_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAppointmentsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid');
+            $table->string('token');
+            $table->foreignId('employee_id')->constrained()->onDelete('cascade');
+            $table->foreignId('service_id')->constrained()->onDelete('cascade');
+            $table->date('date');
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->string('client_name');
+            $table->string('client_email');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('appointments');
+    }
+}
