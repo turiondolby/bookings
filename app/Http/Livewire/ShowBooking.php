f6f9ed75d60b01a51e302e7780595e56c5308190
Haleem Hosein <haleemhosein@yahoo.com>
2022-02-18 19:34:56 -0400

    28 Protecting the booking with a token

--- app/Http/Livewire/ShowBooking.php
+++ app/Http/Livewire/ShowBooking.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\Appointment;
+use Illuminate\Auth\Access\AuthorizationException;
 
 class ShowBooking extends Component
 {
@@ -12,7 +13,12 @@ class ShowBooking extends Component
     public function mount(Appointment $appointment)
     {
         $this->appointment = $appointment;
+
+        if (request()->token !== $appointment->token) {
+            throw new AuthorizationException();
+        }
     }
+
     public function render()
     {
         return view('livewire.show-booking')
